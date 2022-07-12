f07f2fefa864ac2084c6b84d4018b72db0e166ec
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-07-12 14:11:26 -0600

    feat: store textarea input in local storage

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,42 +1,42 @@
-const currentDay = $('#currentDay');
-// textarea selectors
-const textAreaAll = $('.textarea-input');
-const textAreaNine = $('#9am');
-const textAreaTen = $('#10am');
-const textAreaEleven = $('#11am');
-const textAreaTwelve = $('#12pm');
-const textAreaOne = $('#1pm');
-const textAreaTwo = $('#2pm');
-const textAreaThree = $('#3pm');
-const textAreaFour = $('#4pm');
-const textAreaFive = $('#5pm');
-// Save button
-const saveBtn = $('.saveBtn');
+$(document).ready(function () {
+  const currentDay = $('#currentDay');
+  const textAreaInput = document.querySelector('.textarea-input');
 
-var date = moment().format('MMMM Do YYYY, h:mm:ss a');
-currentDay.text(date);
+  init();
 
-$(saveBtn).click(function () {
-  var plannerInputs = {
-    9: textAreaNine.val(),
-    10: textAreaTen.val(),
-    11: textAreaEleven.val(),
-    12: textAreaTwelve.val(),
-    1: textAreaOne.val(),
-    2: textAreaTwo.val(),
-    3: textAreaThree.val(),
-    4: textAreaFour.val(),
-    5: textAreaFive.val(),
-  };
+  var date = moment().format('MMMM Do YYYY, h:mm:ss a');
+  var hour = moment().format('h');
+  currentDay.text(date);
 
-  console.log(plannerInputs);
-  localStorage.setItem('plannerInputs', JSON.stringify(plannerInputs));
+  var plannerDetailsFinal = [];
+
+  $('.saveBtn').click(function (event) {
+    event.preventDefault();
+
+    var planDesc = $(event.target).siblings('.textarea-input').val();
+    var hourTime = $(event.target).parents().attr('id');
+
+    var plannerDetails = {
+      savedPlan: planDesc,
+      timeSlot: hourTime,
+    };
+
+    plannerDetailsFinal.push(plannerDetails);
+
+    localStorage.setItem('plannerDetails', JSON.stringify(plannerDetailsFinal));
+  });
+
+  // $('.saveBtn').click(function (event) {
+  //   event.preventDefault();
+
+  //   var planDesc = $(event.target).siblings('.textarea-input').val();
+  //   var hourTime = $(event.target).parents().attr('id');
+  //   localStorage.setItem('plannerDetails', hourTime, planDesc);
+  // });
+
+  function init() {
+    if (localStorage.getItem('plannerDetails') === null) {
+      console.log('working');
+    }
+  }
 });
-
-var savedPlans = JSON.parse(localStorage.getItem('plannerInputs'));
-console.log(savedPlans);
-// textAreaAll.text(savedPlans);
-
-// var storageDefault = function () {};
-
-// storageDefault();
