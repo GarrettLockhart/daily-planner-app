d6df7e72f4ed9d4d0125c2d7eaafee5295f8cb7a
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-07-13 22:33:41 -0600

    feat: create array based on id of time block div time also dynamically updates

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,42 +1,21 @@
+setInterval(function () {
+  $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+}, 100);
+
 $(document).ready(function () {
-  const currentDay = $('#currentDay');
-  const textAreaInput = document.querySelector('.textarea-input');
-
-  init();
-
-  var date = moment().format('MMMM Do YYYY, h:mm:ss a');
   var hour = moment().format('h');
-  currentDay.text(date);
 
-  var plannerDetailsFinal = [];
-
-  $('.saveBtn').click(function (event) {
-    event.preventDefault();
-
-    var planDesc = $(event.target).siblings('.textarea-input').val();
-    var hourTime = $(event.target).parents().attr('id');
-
-    var plannerDetails = {
-      savedPlan: planDesc,
-      timeSlot: hourTime,
-    };
-
-    plannerDetailsFinal.push(plannerDetails);
-
-    localStorage.setItem('plannerDetails', JSON.stringify(plannerDetailsFinal));
+  $('.saveBtn').on('click', function (event) {
+    var planDesc = $(this).siblings('.textarea-input').val();
+    var hourTime = $(this).parents().attr('id');
+    localStorage.setItem(hourTime, planDesc);
   });
 
-  // $('.saveBtn').click(function (event) {
-  //   event.preventDefault();
+  var IDs = $('#time-block-container span[id]')
+    .map(function () {
+      return this.id;
+    })
+    .get();
 
-  //   var planDesc = $(event.target).siblings('.textarea-input').val();
-  //   var hourTime = $(event.target).parents().attr('id');
-  //   localStorage.setItem('plannerDetails', hourTime, planDesc);
-  // });
-
-  function init() {
-    if (localStorage.getItem('plannerDetails') === null) {
-      console.log('working');
-    }
-  }
+  console.table(IDs);
 });
