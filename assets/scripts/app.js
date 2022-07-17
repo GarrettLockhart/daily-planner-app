3da880302cf983baf66bb17ff0ca5e10694c0d45
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-07-17 14:04:58 -0600

    feat: persit local storage to textareas

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,9 @@ setInterval(function () {
 }, 100);
 
 $(document).ready(function () {
-  var hour = moment().format('h');
+  var currentHour = moment().format('ha');
+  var now = moment().set('hour', 13).format('LT');
+  console.log(currentHour);
 
   $('.saveBtn').on('click', function (event) {
     var planDesc = $(this).siblings('.textarea-input').val();
@@ -11,11 +13,32 @@ $(document).ready(function () {
     localStorage.setItem(hourTime, planDesc);
   });
 
-  var IDs = $('#time-block-container span[id]')
-    .map(function () {
-      return this.id;
-    })
-    .get();
+  function init() {
+    $('#9am').find('.textarea-input').val(localStorage.getItem('9am'));
+    $('#10am').find('.textarea-input').val(localStorage.getItem('10am'));
+    $('#11am').find('.textarea-input').val(localStorage.getItem('11pm'));
+    $('#12am').find('.textarea-input').val(localStorage.getItem('12pm'));
+    $('#1pm').find('.textarea-input').val(localStorage.getItem('1pm'));
+    $('#2pm').find('.textarea-input').val(localStorage.getItem('2pm'));
+    $('#3pm').find('.textarea-input').val(localStorage.getItem('3pm'));
+    $('#4pm').find('.textarea-input').val(localStorage.getItem('4pm'));
+    $('#5pm').find('.textarea-input').val(localStorage.getItem('5pm'));
+  }
 
-  console.table(IDs);
+  function hourColor() {
+    $('.time-block').each(function () {
+      var timeBlockId = $(this).attr('id');
+      console.log(timeBlockId);
+      if (timeBlockId === currentHour) {
+        $('.textarea-input').addClass('present');
+      }
+      if (timeBlockId > currentHour) {
+        $('.textarea-input').addClass('past');
+      }
+    });
+  }
+
+  hourColor();
+
+  init();
 });
