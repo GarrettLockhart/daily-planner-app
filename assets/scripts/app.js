b57658bfdd9de9b82d19c2d92dd020272c4d846e
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-07-17 15:24:56 -0600

    feat: colors change based on hour

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,44 +1,43 @@
 setInterval(function () {
   $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-}, 100);
+}, 500);
 
 $(document).ready(function () {
-  var currentHour = moment().format('ha');
-  var now = moment().set('hour', 13).format('LT');
-  console.log(currentHour);
+  var currentHour = parseInt(moment().format('H'));
 
-  $('.saveBtn').on('click', function (event) {
+  $('.saveBtn').on('click', function () {
     var planDesc = $(this).siblings('.textarea-input').val();
     var hourTime = $(this).parents().attr('id');
     localStorage.setItem(hourTime, planDesc);
   });
 
   function init() {
-    $('#9am').find('.textarea-input').val(localStorage.getItem('9am'));
-    $('#10am').find('.textarea-input').val(localStorage.getItem('10am'));
-    $('#11am').find('.textarea-input').val(localStorage.getItem('11pm'));
-    $('#12am').find('.textarea-input').val(localStorage.getItem('12pm'));
-    $('#1pm').find('.textarea-input').val(localStorage.getItem('1pm'));
-    $('#2pm').find('.textarea-input').val(localStorage.getItem('2pm'));
-    $('#3pm').find('.textarea-input').val(localStorage.getItem('3pm'));
-    $('#4pm').find('.textarea-input').val(localStorage.getItem('4pm'));
-    $('#5pm').find('.textarea-input').val(localStorage.getItem('5pm'));
+    $('#hour-9').find('.textarea-input').val(localStorage.getItem('hour-9'));
+    $('#hour-10').find('.textarea-input').val(localStorage.getItem('hour-10'));
+    $('#hour-11').find('.textarea-input').val(localStorage.getItem('hour-11'));
+    $('#hour-12').find('.textarea-input').val(localStorage.getItem('hour-12'));
+    $('#hour-13').find('.textarea-input').val(localStorage.getItem('hour-13'));
+    $('#hour-14').find('.textarea-input').val(localStorage.getItem('hour-14'));
+    $('#hour-15').find('.textarea-input').val(localStorage.getItem('hour-15'));
+    $('#hour-16').find('.textarea-input').val(localStorage.getItem('hour-16'));
+    $('#hour-17').find('.textarea-input').val(localStorage.getItem('hour-17'));
   }
 
   function hourColor() {
     $('.time-block').each(function () {
-      var timeBlockId = $(this).attr('id');
-      console.log(timeBlockId);
+      var timeBlockId = parseInt($(this).attr('id').split('-')[1]);
       if (timeBlockId === currentHour) {
-        $('.textarea-input').addClass('present');
-      }
-      if (timeBlockId > currentHour) {
-        $('.textarea-input').addClass('past');
+        $(this).addClass('present');
+      } else if (timeBlockId < currentHour) {
+        $(this).removeClass('present');
+        $(this).addClass('past');
+      } else if (timeBlockId > currentHour) {
+        $(this).removeClass('present');
+        $(this).addClass('future');
       }
     });
   }
 
   hourColor();
-
   init();
 });
